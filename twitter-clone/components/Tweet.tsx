39b424a90817208b2e0e66a96f57cd150f7da0aa
Shivang Purrohit <104918730+Shivang-purrohit@users.noreply.github.com>
2022-10-01 17:12:40 +0530

    comments done

--- twitter-clone/components/Tweet.tsx
+++ twitter-clone/components/Tweet.tsx
@@ -61,12 +61,13 @@ useEffect(() => {
 </div>
 {/*comment box logic */}
 {comments?.length > 0 && (
-    <div>
+    <div className='my-2 mt-5 max-h-44 space-y-5 overflow-y-scroll border-t border-gray-100 p-5'>
         {comments.map((comment) => (
-    <div key={comment._id}>
+    <div key={comment._id} className="relative flex space-x-2">
+        <hr className="absolute left-5 top-10 h-8 border-x border-twitter/30" /> 
         <img
         src={comment.profileImg}
-        className='h-7 w-7 rounded-full object-cover' alt='' />
+        className='mt-2 h-7 w-7 rounded-full object-cover' alt='' />
         <div>
             <div className='flex items-center space-x-1'>
                 <p className='mr-1 font-bold'>{comment.username}</p>
@@ -77,10 +78,9 @@ useEffect(() => {
             date={comment._createdAt} />
 
             </div>
-
+            <p>{comment.comment}</p>
            
         </div>
-        <p>{comment.comment}</p>
         </div>
         ))}
         </div>
