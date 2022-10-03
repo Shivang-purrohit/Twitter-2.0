f139d84d1a56bb4476eb771e8f9c454b6c4cc2db
Shivang Purrohit <104918730+Shivang-purrohit@users.noreply.github.com>
2022-10-03 17:48:48 +0530

    Sign in sign out done

--- twitter-clone/components/Sidebar.tsx
+++ twitter-clone/components/Sidebar.tsx
@@ -11,8 +11,12 @@ import {
 
 } from '@heroicons/react/outline'
 import SidebarRow from './SidebarRow'
+import { useSession, signIn , signOut } from 'next-auth/react'
 
 function Sidebar() {
+const {data: session} = useSession()
+
+
   return (
     <div className='col-span-2 flex flex-col items-center px-4 md:items-start'> 
         <img className=' m-3 h-10 w-10' src="https://links.papareact.com/drq" alt='' />
@@ -22,7 +26,7 @@ function Sidebar() {
         <SidebarRow Icon={MailIcon} title='Messages' />
         <SidebarRow Icon={BookmarkIcon} title='Bookmark' />
         <SidebarRow Icon={CollectionIcon} title='Lists' />
-        <SidebarRow Icon={UserIcon} title='Sign In' />
+        <SidebarRow onClick={session ? signOut : signIn } Icon={UserIcon} title={session ? 'Sign Out' : 'Sign In'} />
 
         <SidebarRow Icon={DotsCircleHorizontalIcon} title='More' />
 
